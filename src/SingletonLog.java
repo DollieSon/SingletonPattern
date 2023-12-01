dffe22ab40e7f31fa9fa909822017100f37fff38
DollieSon <62559681+DollieSon@users.noreply.github.com>
2023-12-01 14:59:41 +0800

    Adding Seomthing

--- src/SingletonLog.java
+++ src/SingletonLog.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class SingletonLog {
+public class SingletonLog {
+    private static SingletonLog instance;
+    private int[] arr;
+    private int size;
+    private SingletonLog(){
+        System.out.println("Singleton Created");
+        arr = new int[10];
+        size = 0;
+    }
+    public static SingletonLog getInstance(){
+        if (instance == null) {
+            instance = new SingletonLog();
+        }
+        return instance;
+    }
+    public void addElement(int num){
+        if(size < 10){
+            arr[size++] = num;
+        }else{
+            System.out.println("Arr is full");
+        }
+    }
+
+    public void PrintElems(){
+        for(int x = 0; x< size; x++){
+            System.out.printf(String.valueOf(arr[x]) +  " ");
+        }
+    }
+
 }
